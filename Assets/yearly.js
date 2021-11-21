1afc3ee5b20d80e5868b8ad6d189d1a7191f0c56
JoyObasco <obayemioluwagbemisola@gmail.com>
2021-11-21 23:22:46 +0100

    Commit

---
+++ Assets/yearly.js
@@ -0,0 +1,17 @@
+
+var ctx = document.getElementById('ThreeChart');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            label: '# of Votes',
+            data: [71, 29],
+            backgroundColor: [
+                'rgb(170, 51, 106)',
+                'rgb(70,130,180)',
+            ],
+            borderWidth: 1,
+            cutout: "80%"
+        }]
+    },
+});
